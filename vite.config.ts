243e773cf9b6bb02471191f9e1f0c2fdbfe4501a
priteshsarva <priteshsarva9825@gmail.com>
2025-12-19 22:16:48 +0530

    for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: '/watchpro3/',
+      // base: '/watchpro3/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
